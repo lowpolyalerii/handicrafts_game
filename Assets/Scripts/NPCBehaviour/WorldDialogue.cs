c788dce9ccc575255d2ed72a4bbbc41960cec068
lowpolyalerii <msalexa17@yahoo.com>
2026-02-23 11:50:08 +0000

    DIalogue

--- Assets/Scripts/NPCBehaviour/WorldDialogue.cs
+++ Assets/Scripts/NPCBehaviour/WorldDialogue.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dialogue : MonoBehaviour
+public class WorldDialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
     public string[] lines;
