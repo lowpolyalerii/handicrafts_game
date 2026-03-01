e06b3f50b4792287f52d7d8ee57b9482bdeca7dc
lowpolyalerii <msalexa17@yahoo.com>
2026-03-01 21:03:38 +0000

    Dialogue Nearly Working

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -3,29 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class WorldDialogue : MonoBehaviour
+public class Dialogue : MonoBehaviour
 {
+    public bool canChat = true;
+    public GameObject canvas;
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
+    public NPCTalk npcTalk;
+
     private int index;
 
 
     void Start()
     {
         textComponent.text = string.Empty;
-        StartDialogue();
+        canvas.SetActive(false);
     }
+
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.E)) // || if (Input.GetMouseButtonDown(0))
+        if (npcTalk)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                canChat = false;
+                canvas.SetActive(true);
+                StartDialogue();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.E))
         {
             if (textComponent.text == lines[index])
             {
                 textComponent.text = string.Empty;
                 NextLine();
-                // e appear when sentence finished
             }
             else
             {
@@ -60,7 +74,14 @@ public class WorldDialogue : MonoBehaviour
         }
         else
         {
-            gameObject.SetActive(false);
+            canvas.SetActive(false);
         }
     }
 }
+
+
+//Remove the dialogue script component from the dialogue box (that you followed doing the tutorial)
+//and apply this script component (the script above) to your NPC.
+//Create a SphearColider and place it in front of your NPC and set this as a trigger.
+//Apply the canvas gameobject, the TMPro text field gameobject into the the new variable
+//gameobjects field on your NPC.
