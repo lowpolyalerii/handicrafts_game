4c003a7ca04f2f08a3028d640953ec654e3e7102
lowpolyalerii <msalexa17@yahoo.com>
2026-04-21 15:13:08 +0100

    Start - Beginning - MainWorld Link

---
+++ Assets/Scripts/UI/Settings/StartMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void LinktoWebsite()
+    {
+        Application.OpenURL("https://karimahsparacosm.online/");
+    }
+
+    public void LinktoFeedback()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSeo_pqSlYFZF_ftmWyllxSt_tSBLybxjxUXZC4GZGNF8WiYuA/viewform?usp=dialog");
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Beginning");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+
+    Application.Quit();
+#endif
+        //Application.Quit();
+    }
+}
