b7d910284a43a347cd8e35e169997d9bb6ea6fb4
lowpolyalerii <msalexa17@yahoo.com>
2026-04-28 17:32:19 +0100

    Added to Minigame

---
+++ Assets/Scripts/Mechanics/PromptShuffler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PromptShuffler : MonoBehaviour
+{
+    public string[] lines;
+
+    public TextMeshProUGUI text;
+
+    public TextMeshProUGUI abovetext;
+
+    public GameObject PromptToggle;
+
+
+    public void AccessPrompts()
+    {
+        PromptToggle.gameObject.SetActive(true);
+    }
+
+    public void ConfirmPrompt()
+    {
+        PromptToggle.gameObject.SetActive(false);
+    }
+
+    public void PromptShuffle()
+    {
+        string promptShow = lines[Random.Range(0, lines.Length)];
+        text.text = promptShow;
+        abovetext.text = promptShow;
+    } 
+}
