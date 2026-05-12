9fc373be7f40a8657d21f94c9923193e37e0aec7
lowpolyalerii <msalexa17@yahoo.com>
2026-05-13 00:18:13 +0100

    Added Ending w/ typing effect

---
+++ Assets/Scripts/Dialogue/CutsceneDialogue.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class CutsceneManager : MonoBehaviour
+{
+    public TextAsset inkFile;
+    public TextMeshProUGUI textBox;
+    public GameObject canvas;
+    public Button[] choiceButtons;
+    private int index;
+    [SerializeField] private float typingSpeed = 0.04f;
+    public bool trigger;
+
+    private Coroutine displayLineCoroutine;
+
+    [SerializeField] private GameObject continueIcon;
+
+    private const string PORTRAIT_TAG = "portrait";
+    private const string VIGNETTE_TAG = "vignette";
+
+    [SerializeField] private Animator portraitAnimator;
+    [SerializeField] private Animator vignetteAnimator;
+
+
+    private Story story;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = new Story(inkFile.text);
+        trigger = true;
+        ContinueStory();
+    }
+
+    private void Update()
+    {
+        if ((Input.GetKeyUp(KeyCode.E)) && trigger == true)
+        {
+            ContinueStory();
+        }
+    }
+
+
+    // Update is called once per frame
+    public void ContinueStory()
+    {
+        if (story.canContinue)
+        {
+            canvas.SetActive(true);
+            textBox.gameObject.SetActive(true);
+
+            if (displayLineCoroutine != null) 
+            {
+                StopCoroutine(displayLineCoroutine);
+            }
+
+            displayLineCoroutine = StartCoroutine(DisplayLine(story.Continue()));
+            ShowChoices();
+
+            // handle tags
+            HandleTags(story.currentTags);
+        }
+        else
+        {
+            FinishDialogue();
+        }
+    }
+
+    private IEnumerator DisplayLine(string line)
+    {
+        //empty the dialogue text
+        textBox.text = "";
+
+        //hide items while text is typing
+        continueIcon.SetActive(false);
+
+        //display each letter one at a time
+        foreach(char letter in line.ToCharArray())
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                textBox.text = line;
+                break;
+            }
+            textBox.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        continueIcon.SetActive(true);
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(":");
+            if (splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;
+
+                case VIGNETTE_TAG:
+                    vignetteAnimator.Play(tagValue);
+                    break;
+
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                    break;
+            }
+        }
+    }
+
+    private void ShowChoices()
+    {
+        List<Choice> choices = story.currentChoices;
+        int index = 0;
+        foreach (Choice c in choices)
+        {
+            choiceButtons[index].GetComponentInChildren<TextMeshProUGUI>().text = c.text;
+            continueIcon.gameObject.SetActive(false);
+            choiceButtons[index].gameObject.SetActive(true);
+            index++;
+        }
+        for (int i = index; i < 2; i++)
+        {
+            choiceButtons[i].gameObject.SetActive(false);
+        }
+        textBox.gameObject.SetActive(true);
+    }
+
+    public void SetDecision(int choiceIndex)
+    {
+        story.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
+    }
+
+    private void FinishDialogue()
+    {
+        //trigger = false;
+        //StopAllCoroutines();
+
+        textBox.gameObject.SetActive(false);
+        canvas.SetActive(false);
+        for (int i = 0; i < choiceButtons.Length; i++)
+        {
+            choiceButtons[i].gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/EndingDialogue.ink
@@ -0,0 +1,51 @@
+And again, you're back in the darkness.
+
+Perhaps you could call it your "limbo".
+
+The voices.
+
+How many hours has it been?
+
+You know they care for you, but it feels like these days you don't even matter.
+
+Just another point to bring up in favour of their arguments.
+
+"Why did you buy her oranges, you know she hates oranges?"
+
+You hated them when you were 5. You're 10 now.
+
+It's ridiculous. It's so ridiculous. In the other world, these arguments cease to exist. 
+
+...
+
+There's you. 
+-> YourChoice
+
+== YourChoice ==
+{Stay == 0:
+Your mind is frazzled. 
+
+Should you stay in your world? Should you talk to them?
+}
+
++ [STAY] -> Stay
+
++ [CONFRONT] -> Confront
+
+== Stay ==
+dffdbdbd
+
+bdbdffdbd
+
+{Stay == 2:
+That's right. You feel safer here 
+-> Stay
+}
+
+
+-> DONE
+
+==Confront
+ddffdfdfbdfb
+bdbddbf
+-> DONE
