01b01dd3902452a4b0ed6a2358db85696866139d
lowpolyalerii <msalexa17@yahoo.com>
2026-05-17 04:55:36 +0100

    Added stuff to painting minigame

--- Assets/Scripts/UI/Settings/StartMenu.cs
+++ Assets/Scripts/UI/Settings/StartMenu.cs
@@ -4,11 +4,51 @@ using UnityEngine;
 
 public class StartMenu : MonoBehaviour
 {
-    public GameObject toggle;
+    [SerializeField] private GameObject toggle;
+
+    [SerializeField] private GameObject toggle2;
+
+    [SerializeField] private AudioSource BGM;
+
+    [SerializeField] private GameObject panels;
+
+    [SerializeField] private GameObject drawingsettings;
+
+    [SerializeField] private GameObject prompttext;
+
+    [SerializeField] private GameObject SavedText;
+
+    [SerializeField] private GameObject newPrompt;
 
     public void Continue()
     {
         toggle.gameObject.SetActive(false);
+    }
+
+    public void SaveScreenie()
+    {
+        SavedText.gameObject.SetActive(true);
+        prompttext.gameObject.SetActive(false);
+    }
+
+    public void NotDonePainting()
+    {
+        toggle2.gameObject.SetActive(false);
+        BGM.volume = 1;
+        panels.gameObject.SetActive(true);
+        drawingsettings.gameObject.SetActive(true);
+        prompttext.gameObject.SetActive(true);
+        SavedText.gameObject.SetActive(false);
+        newPrompt.gameObject.SetActive(true);
+    }
+
+    public void DonePainting()
+    {
+        toggle2.gameObject.SetActive(true);
+        BGM.volume = 0;
+        panels.gameObject.SetActive(false);
+        drawingsettings.gameObject.SetActive(false);
+        newPrompt.gameObject.SetActive(false);
     }
 
     public void LinktoWebsite()
